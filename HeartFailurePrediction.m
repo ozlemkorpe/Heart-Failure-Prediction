e0366722395d865e3213feb63dde21a89fc90756
Ozlem <cse.ozlemkorpe@gmail.com>
2021-01-24 17:00:19 +0300

    Accuracy Calculation

--- HeartFailurePrediction.m
+++ HeartFailurePrediction.m
@@ -3,6 +3,13 @@
 %Note1: Please check out the data path before run, fix if necessary.
 %Note1: Change the number of iterations for calculating average/general accuracy of prediction.
 
+%You can change attributes to test other options
+NumOfIterations = 100;
+TestPartition = 0.1 ;
+MaxNumOfSplits = 37 ;
+
+%Create a table to observe results for attributes
+
 data = readtable('Dataset\heart_failure_clinical_records_dataset.csv');
 
 % -----------------------------Column headers
@@ -54,12 +61,12 @@ stand_time = (data.time - mean(data.time)) / std(data.time);
 data.time = stand_time;
 
 %----------------------------- Classification and Prediction
-classification_model = fitctree(data, 'DEATH_EVENT~platelets+serum_creatinine+serum_sodium+sex+smoking+time+age+anaemia+creatinine_phosphokinase+diabetes+ejection_fraction+high_blood_pressure'); 
+classification_model = fitctree(data, 'DEATH_EVENT~platelets+serum_creatinine+serum_sodium+sex+smoking+time+age+anaemia+creatinine_phosphokinase+diabetes+ejection_fraction+high_blood_pressure','MaxNumSplits',MaxNumOfSplits ); 
 
 general_accuracy = 0;
-for a = 1:1000
+for a = 1:NumOfIterations
     %----------------------------- Partition data into training and test set
-    cv = cvpartition(classification_model.NumObservations,'HoldOut', 0.1); %Built-in function for partitioning
+    cv = cvpartition(classification_model.NumObservations,'HoldOut', TestPartition); %Built-in function for partitioning
     cross_validated_model = crossval(classification_model, 'cvpartition', cv); %Use training set only to built model 
 
     %----------------------------- Perform Predictions on test set
@@ -76,6 +83,11 @@ for a = 1:1000
     truth_score = right_results /(right_results + wrong_results);
     general_accuracy = general_accuracy + truth_score;
 
-
 end
 
+    AverageAccuracy = general_accuracy / a; 
+    %Print average accuracy 
+    disp('Average accuracy is:');
+    disp(AverageAccuracy);
+
+    %T = table(NumOfIterations,Partition,MaxNumOfSplits,AverageAccuracy);
